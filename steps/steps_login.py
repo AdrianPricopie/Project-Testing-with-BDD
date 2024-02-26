7fd802c2520fc5fa6b4eeaf452c1cc3e751be355
RobertFurtuna10 <furtuna.robert@yahoo.com>
2024-02-26 21:31:25 +0200

    comentariu

--- steps/steps_login.py
+++ steps/steps_login.py
@@ -6,17 +6,17 @@ from behave import *
 @given('I am on the login page')
 def step_impl(context):
     context.LoginPage.navigate_to_login_page()
-    time.sleep(4)
+    time.sleep(2)
 
 
-@when('I enter a wrong username')
-def step_impl(context):
-    context.LoginPage.enter_wrong_username()
+@when('I enter "{username}" in username field')
+def step_impl(context, username):
+    context.LoginPage.enter_username(username)
 
 
-@when('I enter a wrong password')
-def step_impl(context):
-    context.LoginPage.enter_wrong_password()
+@when('I enter "{password}" in password field')
+def step_impl(context, password):
+    context.LoginPage.enter_password(password)
 
 
 @when('I press login button')
@@ -24,8 +24,29 @@ def step_impl(context):
     context.LoginPage.click_login_button()
 
 
-@then('I should see an error message')
+@then('I should see an internal error message')
 def step_impl(context):
     actual_error_message = context.LoginPage.get_error_message()
     expected_result_message = 'An internal error has occurred and has been logged.'
     assert expected_result_message in actual_error_message
+
+@when('I leave both username and password fields empty')
+def step_impl(context):
+    pass
+
+
+@then('I should see an error message')
+def step_impl(context):
+    actual_error_message = context.LoginPage.get_error_message()
+    expected_result_message = 'Please enter a username and password.'
+    assert actual_error_message in expected_result_message
+
+@then('I should be redirected to the dashboard')
+def step_impl(context):
+    pass
+
+@when('I leave username field empty')
+def step_impl(context):
+    pass
+
+
